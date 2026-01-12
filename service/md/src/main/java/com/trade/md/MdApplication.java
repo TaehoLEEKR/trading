673863e56fd44ba0b97b9a19a467abd4da4680de
TaehoLEEKR <679748@naver.com>
2026-01-12 16:09:13 +0900

    chore: 모의 투자 키라도 위험성이 있다고 하여 제거

---
+++ service/md/src/main/java/com/trade/md/MdApplication.java
@@ -0,0 +1,17 @@
+package com.trade.md;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication(scanBasePackages = {"com.trade.common","com.trade"})
+@EnableJpaAuditing
+@ConfigurationPropertiesScan(basePackages = "com.trade.common.config")
+//@MapperScan("com.trade.catalog.mapper")
+public class MdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MdApplication.class, args);
+    }
+}
