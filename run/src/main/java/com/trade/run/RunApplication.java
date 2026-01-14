8b0d07ea20451509bd623ce53b2d6374b316f527
TaehoLEEKR <679748@naver.com>
2026-01-14 16:20:44 +0900

    chore: run application 구현 / 스케쥴링,배치 시스템으로 사용 할 앱 이라 gracefulShoutdown 을 추가

---
+++ run/src/main/java/com/trade/run/RunApplication.java
@@ -0,0 +1,32 @@
+package com.trade.run;
+
+import com.trade.common.util.Shutdown;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@SpringBootApplication(scanBasePackages = {"com.trade.*"})
+@EnableScheduling
+@EnableAsync
+public class RunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunApplication.class, args);
+    }
+    @Bean
+    public Shutdown gracefulShutdown() {
+        return new Shutdown();
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(final Shutdown gracefulShutdown) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addConnectorCustomizers(gracefulShutdown);
+        return tomcat;
+    }
+}
