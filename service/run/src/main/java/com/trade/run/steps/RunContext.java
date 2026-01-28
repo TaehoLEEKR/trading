87204ae73435fbe57a39fcbf0d48fb78653245de
TaehoLEEKR <679748@naver.com>
2026-01-28 15:38:47 +0900

    feat: RunContext에 withRun 메서드 추가
    - jobKey와 runId를 설정할 수 있는 withRun 메서드 추가
    - RunContext 생성자에 jobKey, runId 필드 추가
    
    todo : 기존 사용하는 context ctx 를 runId 랑 jobKey 를 추가하는 로직으로 변경이 필요함.
    Signed-off-by: TaehoLEEKR <679748@naver.com>

--- service/run/src/main/java/com/trade/run/steps/RunContext.java
+++ service/run/src/main/java/com/trade/run/steps/RunContext.java
@@ -1,4 +1,11 @@
 package com.trade.run.steps;
 
-public record RunContext(String market, String intervalCd, Integer limit, Integer defaultMaxInstruments) {}
+public record RunContext(String market, String intervalCd, Integer limit, Integer defaultMaxInstruments,
+String jobKey, String runId) {
+
+    public RunContext withRun(String jobKey, String runId) {
+        return new RunContext(market, intervalCd, limit, defaultMaxInstruments, jobKey, runId);
+    }
+
+}
 
